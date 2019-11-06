d26c7d8fbfc36d58695fa07ddcdb6bcd88d48d69
Maksym <maksym.kozachuk@protonmail.com>
2019-11-06 17:51:45 +0100

    init,destroy,factory methods + singleton & prototype scope tests

--- src/main/java/com/mkozachuk/spring/ClassicMusic.java
+++ src/main/java/com/mkozachuk/spring/ClassicMusic.java
@@ -1,6 +1,24 @@
 package com.mkozachuk.spring;
 
 public class ClassicMusic implements Music {
+
+    private ClassicMusic(){
+
+    }
+
+    public static ClassicMusic factoryMethod(){
+        return new ClassicMusic();
+    }
+
+
+    public void initMethod(){
+        System.out.println("Do my initialization");
+    }
+
+    public void destroyMethod(){
+        System.out.println("Do my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Symphony No.5";
